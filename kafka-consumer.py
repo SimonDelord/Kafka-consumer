3f3924960a381254b0792173e22c9f34536412ef
SimonDelord <simon.delord@gmail.com>
2024-01-29 11:29:40 +1100

    new python file

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,24 +1,40 @@
 import logging
-from json import loads
-from kafka import KafkaConsumer
+import os
+import json
+
+from dotenv import load_dotenv
+from kafka.consumer import KafkaConsumer
+
+load_dotenv(verbose=True)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main():
+  print("Starting consumer", os.environ["BOOTSTRAP_SERVER"])
+  consumer = KafkaConsumer( 
+    bootstrap_servers=[os.environ["BOOTSTRAP_SERVER"]],
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+ #   group_id=os.environ["CONSUMER_GROUP"],
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+  )
+
+  consumer.subscribe([os.environ["TOPICS_PEOPLE_BASIC_NAME"]])
+
+  for message in consumer:
     try:
-        # To consume latest messages and auto-commit offsets
-        consumer = KafkaConsumer(
-            'acs-topic',
-            bootstrap_servers='172.30.2.176:9092',
-            value_deserializer=lambda x: loads(x.decode('utf-8')),
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-        )
-        for message in consumer:
-            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                 message.offset, message.key, message.value))
+      kafka_message = f"""
+      Message received: {message.value}
+      Message key: {message.key}
+      Message partition: {message.partition}
+      Message offset: {message.offset}
 
+      """
+      logger.info(kafka_message)
     except Exception as e:
-        logging.info('Connection successful', e)
+      logger.error(e)
+  
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+  main()
